731a71f7fb4d13cd385709fd9520e9f160e281de
Jujun Junawan <jujunjunawan2@gmail.com>
2026-05-18 22:15:28 +0700

    fix firebase admin import

--- app/api/midtrans-webhook/route.ts
+++ app/api/midtrans-webhook/route.ts
@@ -1,6 +1,6 @@
 import { NextResponse } from "next/server";
 
-import adminDb from "@/lib/firebase-admin";
+import { adminDb } from "@/lib/firebase-admin";
 
 import { resend } from "@/lib/resend";
 
