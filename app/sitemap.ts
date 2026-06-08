b92effba79e6e985c6646265f9a3f1f210657e9c
Jujun Junawan <jujunjunawan2@gmail.com>
2026-06-08 10:39:54 +0700

    fix sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,15 +1,38 @@
 import { MetadataRoute } from "next";
+import { adminDb } from "@/lib/firebase-admin";
 
-export default function sitemap(): MetadataRoute.Sitemap {
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+
+  const snapshot = await adminDb
+    .collection("products")
+    .get();
+
+  const products = snapshot.docs.map((doc) => {
+    const data = doc.data();
+
+    return {
+      url: `https://www.ks25.my.id/product/${data.slug}`,
+      lastModified: new Date(),
+      changeFrequency: "weekly" as const,
+      priority: 0.9,
+    };
+  });
+
   return [
     {
       url: "https://www.ks25.my.id",
       lastModified: new Date(),
+      changeFrequency: "daily",
+      priority: 1,
     },
 
     {
       url: "https://www.ks25.my.id/tentang-kami",
       lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.8,
     },
+
+    ...products,
   ];
 }
