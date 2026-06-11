8f3317de602118a9fc67642d87c4084c62cf75eb
Jujun Junawan <jujunjunawan2@gmail.com>
2026-06-11 15:31:53 +0700

    add sitemap pagination

--- app/sitemap.xml/route.ts
+++ app/sitemap.xml/route.ts
@@ -1,43 +1,24 @@
 import { NextResponse } from "next/server";
-import { adminDb } from "@/lib/firebase-admin";
-
-export const runtime = "nodejs";
-export const dynamic = "force-dynamic";
-
 
 export async function GET() {
   const baseUrl = "https://www.ks25.my.id";
 
-  const snapshot = await adminDb.collection("products").get();
-
-  const products = snapshot.docs
-    .map((doc) => doc.data())
-    .filter((product) => product.slug)
-    .map(
-      (product) => `
-      <url>
-        <loc>${baseUrl}/product/${product.slug}</loc>
-        <lastmod>${new Date().toISOString()}</lastmod>
-      </url>
-    `
-    );
-
   const xml = `<?xml version="1.0" encoding="UTF-8"?>
-<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+<sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
 
-<url><loc>${baseUrl}</loc></url>
-<url><loc>${baseUrl}/tentang-kami</loc></url>
-<url><loc>${baseUrl}/kebijakan-privasi</loc></url>
-<url><loc>${baseUrl}/syarat-ketentuan</loc></url>
+<sitemap>
+  <loc>${baseUrl}/sitemap-products-1.xml</loc>
+</sitemap>
 
-${products.join("")}
+<sitemap>
+  <loc>${baseUrl}/sitemap-products-2.xml</loc>
+</sitemap>
 
-</urlset>`;
+</sitemapindex>`;
 
-  return new NextResponse(xml, {
+  return new NextResponse(xml.trim(), {
     headers: {
-      "Content-Type": "application/xml",
-      "Cache-Control": "public, s-maxage=3600, stale-while-revalidate=86400"
+      "Content-Type": "application/xml; charset=utf-8",
     },
   });
 }
