406e814136048da08baaa71695c5d4424e947d3e
Jujun Junawan <jujunjunawan2@gmail.com>
2026-06-11 15:55:21 +0700

    fix sitemap2

--- app/sitemap.xml/route.ts
+++ app/sitemap.xml/route.ts
@@ -1,5 +1,9 @@
 import { NextResponse } from "next/server";
 
+export const dynamic = "force-dynamic";
+export const revalidate = 0;
+export const runtime = "nodejs";
+
 export async function GET() {
   const baseUrl = "https://www.ks25.my.id";
 
@@ -17,8 +21,11 @@ export async function GET() {
 </sitemapindex>`;
 
   return new NextResponse(xml.trim(), {
-    headers: {
-      "Content-Type": "application/xml; charset=utf-8",
-    },
-  });
+  headers: {
+    "Content-Type": "application/xml; charset=utf-8",
+
+    // 🔥 penting: MATIKAN CACHE
+    "Cache-Control": "no-store, no-cache, must-revalidate",
+  },
+});
 }
