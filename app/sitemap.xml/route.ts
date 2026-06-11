06421dc5fd558d678f40ea421260c6419c51aeb1
Jujun Junawan <jujunjunawan2@gmail.com>
2026-06-11 15:00:57 +0700

    fix sitemap

--- app/sitemap.xml/route.ts
+++ app/sitemap.xml/route.ts
@@ -1,49 +1,30 @@
 import { NextResponse } from "next/server";
-import { db } from "@/lib/firebase";
-import {
-  collection,
-  getDocs,
-} from "firebase/firestore";
+import { adminDb } from "@/lib/firebase-admin";
 
 export async function GET() {
-  const baseUrl =
-    "https://www.ks25.my.id";
+  const baseUrl = "https://www.ks25.my.id";
 
-  const snapshot = await getDocs(
-    collection(db, "products")
-  );
+  const snapshot = await adminDb.collection("products").get();
 
   const products = snapshot.docs
-  .map((doc) => doc.data())
-  .filter((product) => product.slug)
-  .map(
-    (product) => `
+    .map((doc) => doc.data())
+    .filter((product) => product.slug)
+    .map(
+      (product) => `
       <url>
         <loc>${baseUrl}/product/${product.slug}</loc>
         <lastmod>${new Date().toISOString()}</lastmod>
       </url>
     `
-  );
+    );
 
   const xml = `<?xml version="1.0" encoding="UTF-8"?>
-<urlset
-xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
 
-<url>
-<loc>${baseUrl}</loc>
-</url>
-
-<url>
-<loc>${baseUrl}/tentang-kami</loc>
-</url>
-
-<url>
-<loc>${baseUrl}/kebijakan-privasi</loc>
-</url>
-
-<url>
-<loc>${baseUrl}/syarat-ketentuan</loc>
-</url>
+<url><loc>${baseUrl}</loc></url>
+<url><loc>${baseUrl}/tentang-kami</loc></url>
+<url><loc>${baseUrl}/kebijakan-privasi</loc></url>
+<url><loc>${baseUrl}/syarat-ketentuan</loc></url>
 
 ${products.join("")}
 
@@ -51,8 +32,7 @@ ${products.join("")}
 
   return new NextResponse(xml, {
     headers: {
-      "Content-Type":
-        "application/xml",
+      "Content-Type": "application/xml",
     },
   });
 }
