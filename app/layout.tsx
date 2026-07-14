1a3a3717d89adc45f48ddff57dcd8f2b3e315424
Jujun Junawan <jujunjunawan2@gmail.com>
2026-07-14 09:50:32 +0700

    edit ga4

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import "./globals.css";
 import { AuthProvider } from "@/components/providers/AuthProvider";
 import type { Metadata } from "next";
 import Script from "next/script";
+import { GoogleAnalytics } from "@next/third-parties/google";
 
 export const metadata: Metadata = {
   metadataBase: new URL("https://www.ks25.my.id"),
@@ -57,20 +58,7 @@ export default function RootLayout({
   return (
     <html lang="id">
       <body>
-        <Script
-          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}`}
-          strategy="afterInteractive"
-        />
-
-        <Script id="google-analytics" strategy="afterInteractive">
-          {`
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-
-            gtag('config', '${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}');
-          `}
-        </Script>
+        <GoogleAnalytics gaId="G-T41M3WJYL9" />
         <AuthProvider>{children}</AuthProvider>
       </body>
     </html>
