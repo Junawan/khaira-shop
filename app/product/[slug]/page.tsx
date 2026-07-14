a7d82fe9c74462f7ca34837881cc597ebf73d139
Jujun Junawan <jujunjunawan2@gmail.com>
2026-07-14 11:03:21 +0700

    fixmd

--- app/product/[slug]/page.tsx
+++ app/product/[slug]/page.tsx
@@ -21,34 +21,29 @@ export async function generateMetadata({
   }
 
   const image =
-    product.images?.[0] ??
-    product.image ??
-    "/logo.png";
+    product.images?.[0] ||
+    product.image ||
+    "https://www.ks25.my.id/logo.png";
 
-  const title = `${product.name} | KhairaShop25`;
-
-  const description =
-    product.description ??
-    `${product.name} tersedia di KhairaShop25.`;
-
-  const url =
-    `https://www.ks25.my.id/product/${product.slug}`;
+  const url = `https://www.ks25.my.id/product/${product.slug}`;
 
   return {
-    title,
+    title: `${product.name} | KhairaShop25`,
 
-    description,
+    description:
+      product.description?.slice(0, 160) ??
+      `${product.name} tersedia di KhairaShop25.`,
 
     alternates: {
       canonical: url,
     },
 
     openGraph: {
-      title,
-      description,
-      url,
       type: "website",
-
+      url,
+      title: product.name,
+      description:
+        product.description?.slice(0, 160),
       images: [
         {
           url: image,
@@ -58,11 +53,9 @@ export async function generateMetadata({
 
     twitter: {
       card: "summary_large_image",
-
-      title,
-
-      description,
-
+      title: product.name,
+      description:
+        product.description?.slice(0, 160),
       images: [image],
     },
   };
